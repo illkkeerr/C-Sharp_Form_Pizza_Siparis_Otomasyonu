80d41dc468ad06ac504f9783d9d7600dfab5af11
illkkeerr <ilkersenel5797@gmail.com>
2023-04-27 02:38:20 +0300

    C-Sharp_Form_Pizza_Siparis_Otomasyonu
    C# Form Pizza Sipariş Otomasyonu

---
+++ Pizza_Otomasyonu/KenarTip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaOtomasyonu
+{
+    internal class KenarTip
+    {
+        public string Adi { get; set; }
+        public decimal EkFiyat { get; set; }
+
+        
+    }
+}
